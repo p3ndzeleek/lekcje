9966301144ac7ae718c33668175f7b7cecd84551
p3ndzeleek <114579263+p3ndzeleek@users.noreply.github.com>
2024-02-02 11:07:02 +0000

    pierwsze gowno

--- lekcje/zadlekcja.py
+++ lekcje/zadlekcja.py
@@ -1,12 +1,31 @@
-a = input()
-b = input()
-X, Y = list(a), list(b)
-X.sort()
-Y.sort()
-a ="".join(X)
-b ="".join(Y)
-print(a,b)
-if a == b:
-    print("tak")
-else:
-    print("nie")
+# liczba  = [random.uniform(0,50) for i in range (20)]
+# print("Tablica liczb ", liczba)
+
+# nieparzysta = any(liczba % 2! = 0 for liczba in liczba)
+# print(nieparzysta)
+
+
+# #do tabeli 6 owocow - pytajac sie za kazdym razem uzytkownika o jego ulubiony owoc
+# owoce = []
+# for i in range(6):
+#     owoce1 = input("Podaj ulubiony owoc: ")
+#     owoce.append(owoce1)
+
+# #w nowej tabeli wypisz ile ma znakow kazdy z owocow
+
+# dlugosc = [len(owoc) for owoc in owoce]
+# print(owoce, dlugosc)
+
+#anagram
+# a = input()
+# b = input()
+# X, Y = list(a), list(b)
+# X.sort()
+# Y.sort()
+# a ="".join(X)
+# b ="".join(Y)
+# print(a,b)
+# if a == b:
+#     print("tak")
+# else:
+#     print("nie")
